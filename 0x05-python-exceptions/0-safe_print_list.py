b814da1a48548445bb4b819483e56e341cf4204a
lynlinah <carolinanyakio12@gmail.com>
2023-06-26 12:25:34 -0700

    correction

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    count = 0
-    for i in range (x):
+    counter = 0
+    for i in range(x):
         try:
-            print("{}" .format(my_list[i]), end="")
-            count += 1
+            print("{}".format(my_list[i]), end="")
         except IndexError:
-            pass
-        finally:
-            print()
-            return count
+            break
+        else:
+            counter += 1
+    print()
+    return counter
